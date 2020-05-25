0d2f43be7f4bf87f87a802aa3e571d2bf25df72c
alexisFdz19 <alexisFdz_19@outlook.com>
2020-05-25 17:51:32 -0500

    Portafolio
    -Agregado banner portafolio
    -Nuevo diseño de proyectos en portafolio

---
+++ js/videoPortafolio.js
@@ -0,0 +1,139 @@
+var $video = $('.videoPortafolio');
+
+$video.on('mouseenter focus', function()   {
+
+    $video.get(0).play();
+
+});
+
+$video.on('mouseout blur', function()   {
+
+    $video.get(0).pause();
+
+});
+
+var $video2 = $('.videoPortafolio2');
+
+$video2.on('mouseenter focus', function()   {
+
+    $video2.get(0).play();
+
+});
+
+$video2.on('mouseout blur', function()   {
+
+    $video2.get(0).pause();
+
+});
+
+var $video3 = $('.videoPortafolio3');
+
+$video3.on('mouseenter focus', function()   {
+
+    $video3.get(0).play();
+
+});
+
+$video3.on('mouseout blur', function()   {
+
+    $video3.get(0).pause();
+
+});
+
+/* var $video4 = $('.videoPortafolio4');
+
+$video4.on('mouseenter focus', function()   {
+
+    $video4.get(0).play();
+
+}); 
+
+$video4.on('mouseout blur', function()   {
+
+    $video4.get(0).pause();
+
+}); */
+
+var $video5 = $('.videoPortafolio5');
+
+$video5.on('mouseenter focus', function()   {
+
+    $video5.get(0).play();
+
+});
+
+$video5.on('mouseout blur', function()   {
+
+    $video5.get(0).pause();
+
+});
+
+var $video6 = $('.videoPortafolio6');
+
+$video6.on('mouseenter focus', function()   {
+
+    $video6.get(0).play();
+
+});
+
+$video6.on('mouseout blur', function()   {
+
+    $video6.get(0).pause();
+
+});
+
+var $video7 = $('.videoPortafolio7');
+
+$video7.on('mouseenter focus', function()   {
+
+    $video7.get(0).play();
+
+});
+
+$video7.on('mouseout blur', function()   {
+
+    $video7.get(0).pause();
+
+});
+
+var $video8 = $('.videoPortafolio8');
+
+$video8.on('mouseenter focus', function()   {
+
+    $video8.get(0).play();
+
+});
+
+$video8.on('mouseout blur', function()   {
+
+    $video8.get(0).pause();
+
+});
+
+var $video9 = $('.videoPortafolio9');
+
+$video9.on('mouseenter focus', function()   {
+
+    $video9.get(0).play();
+
+});
+
+$video9.on('mouseout blur', function()   {
+
+    $video9.get(0).pause();
+
+});
+
+var $video10 = $('.videoPortafolio10');
+
+$video10.on('mouseenter focus', function()   {
+
+    $video10.get(0).play();
+
+});
+
+$video10.on('mouseout blur', function()   {
+
+    $video10.get(0).pause();
+
+});
