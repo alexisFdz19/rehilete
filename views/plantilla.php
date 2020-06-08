94d17743ebb182a18553b059f3e44baaf0b63da2
alexisFdz19 <alexisFdz_19@outlook.com>
2020-06-08 15:22:59 -0500

    Google
    -Eliminada etiqueta de verificación

--- views/plantilla.php
+++ views/plantilla.php
@@ -20,7 +20,6 @@
     <meta content="width=device-width, initial-scale=1" name="viewport">
     <meta content="Webflow" name="generator">
     <meta name="theme-color" content="#19C1DD"/>
-    <meta name="google-site-verification" content="daurpHRLtoN9JNRDjNCDsTprdVBtq4tu_EeMceD38bk" />
 
     <!-- **Font Awesome** -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.9.0/css/all.css" integrity="sha256-PF6MatZtiJ8/c9O9HQ8uSUXr++R9KBYu4gbNG5511WE=" crossorigin="anonymous" />
